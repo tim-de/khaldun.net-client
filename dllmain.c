685f5568f2d65eaef3c4455da01bc054d1ae90ac
Tim <evanstde@gmail.com>
2025-02-10 21:34:48 +0000

    Embed ping function in dllmain rather than trying to link other file

--- dllmain.c
+++ dllmain.c
@@ -54,8 +54,6 @@
 #include "include/picoupnp.h"
 #include "iathook/iathook.h"
 
-#include "include/ping-khaldun.h"
-
 // Redirect all bind() to 0.0.0.0
 static int force_bind_ip = 1;
 
@@ -180,6 +178,58 @@ unsigned long __stdcall teredoThread(void* param) {
 
 __forceinline static void DisableTeredoTunneling(void) {
   CloseHandle(CreateThread(0, 0, teredoThread, 0, 0, 0));
+}
+
+int ping_khaldun() {
+    WSADATA wsadata;
+    SOCKET sock;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    char *server_ip;
+    int count = 0;
+    int result;
+    char buf[PING_BUFSIZE];
+
+    result = WSAStartup(MAKEWORD(2, 2), &wsadata);
+    if (result != NO_ERROR) {
+        return 0;
+    }
+    
+    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock == INVALID_SOCKET) {
+        WSACleanup();
+        return 0;
+    }
+
+    server = gethostbyname(khaldun_url);
+    server_ip = inet_ntoa (*(struct in_addr *) *server->h_addr_list);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(server_ip);
+    serv_addr.sin_port = TCP_PORT;
+
+    result = bind(sock, (SOCKADDR *) &serv_addr, sizeof(serv_addr));
+    if (result = SOCKET_ERROR) {
+        closesocket(sock);
+        WSACleanup();
+        return 0;
+    }
+
+    DWORD timeout = 1500;
+    int timeoutlen = sizeof(DWORD);
+    result = setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, &timeoutlen);
+    if (result == SOCKET_ERROR) {
+        closesocket(sock);
+        WSACleanup();
+        return 0;
+    }
+
+    result = recv(sock, buf, PING_BUFSIZE, 0);
+    result = (result < 1) ? 1 : 0;
+
+    closesocket(sock);
+    WSACleanup();
+    return result;
 }
 
 static volatile int initialized = 0;
